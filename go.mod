6b1923c21b0e6f65ead69002f22bad1adcfb3334
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-04-13 15:52:02 +0900

    Add MyQL

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/YadaYuki/go-echo-docker
 go 1.15
 
 require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"net/http"
+	"database/sql"
 	"github.com/labstack/echo"
+	// "github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"fmt"
+	"log"
 )
 
 
 func main(){
+	DB_HOST := "database"
+	DB_PORT := "3306"
+	DB_USER := "user"
+	DB_PASS := "password"
+	DB_NAME := "sampledb"
+	connectionString :=fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",DB_USER, DB_PASS,DB_HOST, DB_PORT, DB_NAME)
+	log.Println(DB_USER+","+DB_PASS)
+	_, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected!")
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")

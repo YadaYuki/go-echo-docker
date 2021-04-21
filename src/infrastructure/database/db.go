6bf673aeb32fddf157b27e88584d37950e13f73d
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-04-21 16:30:27 +0900

    Fixed from  to

--- src/infrastructure/database/db.go
+++ src/infrastructure/database/db.go
@@ -16,9 +16,14 @@ func New() database.SqlHandler {
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local", env.DB_USER, env.DB_PASSWORD, env.DB_HOST, env.DB_PORT, env.DB_NAME)
 	connection, err := sql.Open("mysql", connectionString)
 	if err != nil {
-		panic(err.Error)
+		panic(err.Error())
 	}
 	// defer connection.Close()
+
+	if err := connection.Ping();err != nil{
+		panic(err.Error())
+	}
+
 	sqlHandler := new(SqlHandler)
 	sqlHandler.Conn = connection
 	return sqlHandler

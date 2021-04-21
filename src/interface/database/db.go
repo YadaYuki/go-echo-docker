d85ead8f34c69ec9ef1e8d6bfd60a5b381e126b3
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-04-21 19:35:03 +0900

    Delete

--- src/interface/database/db.go
+++ src/interface/database/db.go
@@ -6,23 +6,5 @@ import (
 
 type SqlHandler interface {
 	First(interface{}, ...interface{}) *gorm.DB
-	// Query(string, ...interface{}) (Row, error)
 	Create(interface{}) *gorm.DB
 }
-
-// type Result interface {
-// 	LastInsertId() (int64, error)
-// 	RowsAffected() (int64, error)
-// }
-
-// type DB struct {
-// 	Error        error
-// 	RowsAffected int64
-// 	clone        int
-// }
-
-// type Row interface {
-// 	Scan(...interface{}) error
-// 	Next() bool
-// 	Close() error
-// }
